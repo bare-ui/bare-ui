860161e927b94e10f182993c4c4deb67c5b97b18
Jerald Austero <jerald.austero0229@gmail.com>
2026-04-06 22:20:54 +0800

    chore: add container style in every component preview

--- packages/wire-ui/.storybook/preview.ts
+++ packages/wire-ui/.storybook/preview.ts
@@ -1,19 +1,37 @@
-import type { Preview } from '@storybook/react-vite'
-import './tailwind.css'
+import type { Preview } from '@storybook/react-vite';
+import { createElement } from 'react';
+import './tailwind.css';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
+	decorators: [
+		(Story) =>
+			createElement(
+				'div',
+				{
+					style: {
+						maxWidth: '1000px',
+						width: '100%',
+						margin: '0 auto',
+						padding: '32px 24px',
+						border: '1px dashed #e0e0e0',
+						fontFamily: "'Patrick Hand', cursive",
+					},
+				},
+				createElement(Story),
+			),
+	],
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
 
-    a11y: {
-      test: 'todo',
-    },
-  },
-}
+		a11y: {
+			test: 'todo',
+		},
+	},
+};
 
-export default preview
+export default preview;
