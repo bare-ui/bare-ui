ff1884e789e26e3eb07e33f223cbc6d1966b7f69
Jerald Austero <jerald.austero0229@gmail.com>
2026-04-09 10:09:44 +0800

    chore: re-organize the sidebar

--- apps/docs/content/_meta.ts
+++ apps/docs/content/_meta.ts
@@ -1,10 +1,36 @@
 export default {
+  '-- overview': {
+    type: 'separator',
+    title: 'Overview',
+  },
   index: 'Introduction',
   'getting-started': 'Getting Started',
+  faq: 'FAQ',
+  'data-attributes': 'Data Attributes',
+  'browser-support': 'Browser Support',
+  changelog: {
+    title: 'Changelog',
+    href: 'https://github.com/wire-ui/wire-ui/releases',
+  },
+  '-- ai': {
+    type: 'separator',
+    title: 'AI for Agents',
+  },
+  ai: {
+    title: 'AI',
+    display: 'children',
+  },
+  '-- api-reference': {
+    type: 'separator',
+    title: 'API Reference',
+  },
   components: 'Components',
   hooks: 'Hooks',
-  'data-attributes': 'Data Attributes',
-  ai: 'AI',
+  '-- resources': {
+    type: 'separator',
+    title: 'Resources',
+  },
+  security: 'Security',
   author: 'Author',
   license: 'License',
   'code-of-conduct': 'Code of Conduct',
