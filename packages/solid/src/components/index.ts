424b6dad30c8fbb498d8237e7021d26f2442a459
Jerald Austero <jerald.austero0229@gmail.com>
2026-04-25 12:16:52 +0800

    chore: export components

---
+++ packages/solid/src/components/index.ts
@@ -0,0 +1,4 @@
+export { Accordion } from './accordion';
+export { Card } from './card';
+export { Divider } from './divider';
+export { List } from './list';
--- packages/solid/src/index.ts
+++ packages/solid/src/index.ts
@@ -1,2 +1,22 @@
+// Components
+export { Accordion, Card, Divider, List } from './components';
+
+// Primitives
+export { createInteractiveState } from './primitives';
+export type { InteractiveStateOptions, InteractiveStateResult } from './primitives';
+
 // Types
 export type { Size, Status, HorizontalPosition, BaseFormFieldProps, BaseOption } from './types';
+
+// Component types
+export type {
+	AccordionRootProps,
+	AccordionRootSingleProps,
+	AccordionRootMultipleProps,
+	AccordionItemProps,
+	AccordionTriggerProps,
+	AccordionContentProps,
+} from './components/accordion';
+export type { CardProps } from './components/card';
+export type { DividerProps } from './components/divider';
+export type { ListProps } from './components/list';
