004e0f5190a478a0236da97dbfd3eb62ede84928
Jerald Austero <jerald.austero0229@gmail.com>
2026-04-27 15:41:17 +0800

    chore: update config to add new primi click outside

--- packages/solid/src/primitives/index.ts
+++ packages/solid/src/primitives/index.ts
@@ -1,2 +1,3 @@
+export { createClickOutside } from './create-click-outside';
 export { createInteractiveState } from './create-interactive-state';
 export type { InteractiveStateOptions, InteractiveStateResult } from './create-interactive-state';
