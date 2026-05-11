59fd661ea359cf7605f9c76c1123992de3d5cbc3
Jerald Austero <jerald.austero0229@gmail.com>
2026-05-11 22:28:57 +0800

    chore: add new components

--- packages/solid/src/index.ts
+++ packages/solid/src/index.ts
@@ -2,19 +2,24 @@
 export {
 	Accordion,
 	Alert,
+	AspectRatio,
 	Avatar,
 	Badge,
+	Breadcrumb,
 	Button,
 	Card,
 	Checkbox,
+	Combobox,
 	Divider,
 	Drawer,
 	Dropdown,
+	FileUpload,
 	Icon,
 	Image,
 	Input,
 	List,
 	Modal,
+	NumberInput,
 	OTP,
 	Password,
 	ProgressBar,
@@ -23,9 +28,14 @@ export {
 	Search,
 	Select,
 	Switch,
+	Tag,
+	TagInput,
 	Textarea,
 	Timeago,
+	Toast,
+	useToast,
 	Tooltip,
+	TreeView,
 } from './components';
 
 // Icon constants
@@ -48,13 +58,16 @@ export type {
 	AccordionContentProps,
 } from './components/accordion';
 export type { AlertRootProps, AlertTitleProps, AlertDescriptionProps, AlertDismissProps } from './components/alert';
-export type {
-	AvatarRootProps,
-	AvatarImageProps,
-	AvatarFallbackProps,
-	AvatarImageStatus,
-} from './components/avatar';
+export type { AspectRatioProps } from './components/aspect-ratio';
+export type { AvatarRootProps, AvatarImageProps, AvatarFallbackProps, AvatarImageStatus } from './components/avatar';
 export type { BadgeProps } from './components/badge';
+export type {
+	BreadcrumbRootProps,
+	BreadcrumbListProps,
+	BreadcrumbItemProps,
+	BreadcrumbLinkProps,
+	BreadcrumbSeparatorProps,
+} from './components/breadcrumb';
 export type { ButtonProps } from './components/button';
 export type { CardProps } from './components/card';
 export type {
@@ -63,6 +76,16 @@ export type {
 	CheckboxIndicatorProps,
 	CheckboxLabelProps,
 } from './components/checkbox';
+export type {
+	ComboboxOption,
+	ComboboxRootProps,
+	ComboboxInputProps,
+	ComboboxTriggerProps,
+	ComboboxContentProps,
+	ComboboxEmptyProps,
+	ComboboxItemRenderProps,
+	ComboboxItemsProps,
+} from './components/combobox';
 export type { DividerProps } from './components/divider';
 export type {
 	DrawerRootProps,
@@ -78,6 +101,12 @@ export type {
 	DropdownMenuProps,
 	DropdownPosition,
 } from './components/dropdown';
+export type {
+	FileUploadRootProps,
+	FileUploadInputProps,
+	FileUploadTriggerProps,
+	FileUploadDropzoneProps,
+} from './components/file-upload';
 export type { IconName, IconSize, IconProps } from './components/icon';
 export type { ImageProps } from './components/image';
 export type { InputRootProps, InputFieldProps, InputLabelProps, InputErrorProps } from './components/input';
@@ -89,6 +118,12 @@ export type {
 	ModalContentProps,
 	ModalCloseProps,
 } from './components/modal';
+export type {
+	NumberInputRootProps,
+	NumberInputFieldProps,
+	NumberInputIncrementProps,
+	NumberInputDecrementProps,
+} from './components/number-input';
 export type { OTPRootProps, OTPSlotProps, OTPSeparatorProps } from './components/otp';
 export type {
 	PasswordRootProps,
@@ -119,6 +154,8 @@ export type {
 	SelectGroupLabelProps,
 } from './components/select';
 export type { SwitchRootProps, SwitchThumbProps } from './components/switch';
+export type { TagRootProps, TagLabelProps, TagRemoveProps } from './components/tag';
+export type { TagInputRootProps, TagInputListProps, TagInputFieldProps } from './components/tag-input';
 export type {
 	TextareaRootProps,
 	TextareaFieldProps,
@@ -126,4 +163,20 @@ export type {
 	TextareaErrorProps,
 } from './components/textarea';
 export type { TimeagoProps, TimeagoFormatConfig, TimeagoPlural } from './components/timeago';
+export type {
+	ToastStatus,
+	ToastData,
+	ToastProviderProps,
+	ToastViewportProps,
+	ToastRootProps,
+	ToastTitleProps,
+	ToastDescriptionProps,
+	ToastCloseProps,
+} from './components/toast';
 export type { TooltipRootProps, TooltipTriggerProps, TooltipContentProps } from './components/tooltip';
+export type {
+	TreeNode,
+	TreeSelectionMode,
+	TreeItemState,
+	TreeViewRootProps,
+} from './components/tree-view';
