a4776591cacc5abb383052bd2fb3f4c32062f6d6
Jerald Austero <jerald.austero0229@gmail.com>
2026-05-17 20:34:40 +0800

    chore: Import all new components

--- apps/docs/src/mdx-components.tsx
+++ apps/docs/src/mdx-components.tsx
@@ -5,30 +5,52 @@ import { LicenseText } from './components/license-text'
 import { Preview } from './components/preview'
 import { AccordionBasic as AccordionPreview } from './components/previews/accordion'
 import { AlertComplex as AlertPreview } from './components/previews/alert'
+import { AspectRatioPreview } from './components/previews/aspect-ratio'
 import { AvatarPreview } from './components/previews/avatar'
 import { BadgeBasic as BadgePreview } from './components/previews/badge'
+import { BreadcrumbPreview } from './components/previews/breadcrumb'
 import { ButtonBasic as ButtonPreview } from './components/previews/button'
+import { CalendarPreview } from './components/previews/calendar'
 import { CardComposed as CardPreview } from './components/previews/card'
 import { CheckboxComposed as CheckboxPreview } from './components/previews/checkbox'
+import { ComboboxPreview } from './components/previews/combobox'
+import { ContextMenuPreview } from './components/previews/context-menu'
+import { DatePickerPreview } from './components/previews/date-picker'
 import { DividerPreview } from './components/previews/divider'
 import { Basic as DrawerPreview } from './components/previews/drawer'
 import { Basic as DropdownPreview } from './components/previews/dropdown'
+import { FileUploadPreview } from './components/previews/file-upload'
+import { FormPreview } from './components/previews/form'
 import { IconPreview } from './components/previews/icon'
 import { Basic as ImagePreview } from './components/previews/image'
 import { InputBasic as InputPreview } from './components/previews/input'
 import { ListComplex as ListPreview } from './components/previews/list'
+import { MenuBarPreview } from './components/previews/menu-bar'
 import { Composed as ModalPreview } from './components/previews/modal'
+import { NavigationMenuPreview } from './components/previews/navigation-menu'
+import { NumberInputPreview } from './components/previews/number-input'
 import { OtpComplex as OtpPreview } from './components/previews/otp'
+import { PaginationPreview } from './components/previews/pagination'
 import { PasswordComplex as PasswordPreview } from './components/previews/password'
+import { PopoverPreview } from './components/previews/popover'
 import { ProgressBarComplex as ProgressBarPreview } from './components/previews/progress-bar'
 import { RadioComplex as RadioPreview } from './components/previews/radio'
 import { RatingComplex as RatingPreview } from './components/previews/rating'
+import { ResizablePanelsPreview } from './components/previews/resizable-panels'
 import { SearchBasic as SearchPreview } from './components/previews/search'
 import { SelectBasic as SelectPreview } from './components/previews/select'
+import { SkeletonPreview } from './components/previews/skeleton'
+import { SliderPreview } from './components/previews/slider'
+import { SpinnerPreview } from './components/previews/spinner'
 import { SwitchComposed as SwitchPreview } from './components/previews/switch'
+import { TabsPreview } from './components/previews/tabs'
+import { TagPreview } from './components/previews/tag'
+import { TagInputPreview } from './components/previews/tag-input'
 import { TextareaBasic as TextareaPreview } from './components/previews/textarea'
 import { Composed as TimeagoPreview } from './components/previews/timeago'
+import { ToastPreview } from './components/previews/toast'
 import { Basic as TooltipPreview } from './components/previews/tooltip'
+import { TreeViewPreview } from './components/previews/tree-view'
 
 export function useMDXComponents(components?: Record<string, React.FC>) {
   return getDocsMDXComponents({
@@ -41,30 +63,52 @@ export function useMDXComponents(components?: Record<string, React.FC>) {
     Preview,
     AccordionPreview,
     AlertPreview,
+    AspectRatioPreview,
     AvatarPreview,
     BadgePreview,
+    BreadcrumbPreview,
     ButtonPreview,
+    CalendarPreview,
     CardPreview,
     CheckboxPreview,
+    ComboboxPreview,
+    ContextMenuPreview,
+    DatePickerPreview,
     DividerPreview,
     DrawerPreview,
     DropdownPreview,
+    FileUploadPreview,
+    FormPreview,
     IconPreview,
     ImagePreview,
     InputPreview,
     ListPreview,
+    MenuBarPreview,
     ModalPreview,
+    NavigationMenuPreview,
+    NumberInputPreview,
     OtpPreview,
+    PaginationPreview,
     PasswordPreview,
+    PopoverPreview,
     ProgressBarPreview,
     RadioPreview,
     RatingPreview,
+    ResizablePanelsPreview,
     SearchPreview,
     SelectPreview,
+    SkeletonPreview,
+    SliderPreview,
+    SpinnerPreview,
     SwitchPreview,
+    TabsPreview,
+    TagPreview,
+    TagInputPreview,
     TextareaPreview,
     TimeagoPreview,
+    ToastPreview,
     TooltipPreview,
+    TreeViewPreview,
     ...components,
   })
 }
