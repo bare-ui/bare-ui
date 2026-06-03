3407fc7252dae3b3f54ad823f35bcc449de95fbe
Jerald Austero <jerald.austero0229@gmail.com>
2026-06-03 22:56:34 +0800

    chore: Remove dead utility for generting random UUID

--- packages/react/src/utils/helper.ts
+++
@@ -1,8 +0,0 @@
-export const Helper = {
-	generateUUID(): string {
-		const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-		const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)];
-		const newDate = new Date().getTime();
-		return `${randomCharacter}${Math.random().toString(32).substring(2)}${newDate}`;
-	},
-};
--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -1,2 +1,1 @@
-export { Helper } from './helper';
 export { mergeProps } from './merge-props';
